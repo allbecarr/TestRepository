277b4dfad8fbafab3b6336680d2560fce11270e2
allbecarr <allbecarr@gmail.com>
2024-03-27 12:58:20 -0300

    Segundo Comentário

--- Teste/obj/Debug/net8.0/Teste.AssemblyInfo.cs
+++ Teste/obj/Debug/net8.0/Teste.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Teste")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf0394c5f4b4e53f3ffdbb91331c8b7983238672")]
 [assembly: System.Reflection.AssemblyProductAttribute("Teste")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Teste")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
